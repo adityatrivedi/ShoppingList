429c2548835aef1cb1655229ee11f42ccf189bd1
Aditya Trivedi <adityatriv@gmail.com>
2014-08-31 22:29:56 +0530

    Add an item to the shopping list.

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,8 @@ def show_help():
   print("What should we pick up at the store?")
   print("Enter DONE to stop. Enter HELP for this help. Enter SHOW to see your current list.")
 
+def add_to_list(item):
+  shopping_list.append(item)
+  print("Added! List has {} items.".format(len(shopping_list)))
+
+
