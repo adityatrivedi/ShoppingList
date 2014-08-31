07e995b5284dd95bca99045b50a9009e70dbdbed
Aditya Trivedi <adityatriv@gmail.com>
2014-08-31 22:25:01 +0530

    Add method containg help messages.

---
+++ shopping_list.py
@@ -0,0 +1,6 @@
+shopping_list = []
+
+def show_help():
+  print("What should we pick up at the store?");
+  print("Enter DONE to stop. Enter HELP for this help. Enter SHOW to see your current list.");
+
