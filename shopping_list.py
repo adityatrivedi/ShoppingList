1250b212174d0993c2316baa46bca3036f06059d
Aditya Trivedi <adityatriv@gmail.com>
2014-08-31 22:43:34 +0530

    Implement main loop.
    Take input from user and add it to the shopping list. Display help
    messages and contents of the list. Terminate program when DONE is
    inputted.

--- shopping_list.py
+++ shopping_list.py
@@ -13,3 +13,17 @@ def show_list():
   for item in shopping_list:
     print(item)
 
+show_help()
+
+while True:
+  new_item = input("> ")
+  if new_item == 'DONE':
+    break
+  elif new_item == 'HELP':
+    show_help()
+    continue
+  elif new_item == 'SHOW':
+    show_list()
+    continue
+  add_to_list(new_item)
+  continue
