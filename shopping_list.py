669e6854e0bf6eefe640841c6aa2a2cf4bb7a0be
Aditya Trivedi <adityatriv@gmail.com>
2014-09-01 19:23:50 +0530

    Follow Google python style guide for variables.

--- shopping_list.py
+++ shopping_list.py
@@ -2,19 +2,19 @@
 
 import sys
 
-shoppingList = []
+shopping_list = []
 
 def show_help():
     print("What should we pick up at the store?")
     print("Enter DONE to stop. Enter HELP for this help. Enter SHOW to see your current list.")
 
 def add_to_list(item):
-    shoppingList.append(item)
-    print("Added! List has {} items.".format(len(shoppingList)))
+    shopping_list.append(item)
+    print("Added! List has {} items.".format(len(shopping_list)))
 
 def show_list():
     print("Here's your list:")
-    for item in shoppingList:
+    for item in shopping_list:
         print(item)
 
 def save_to_file(textList):
@@ -23,14 +23,13 @@ def save_to_file(textList):
         outfile.write("%s\n" % item)
     outfile.close
 
-
 show_help()
 
 while True:
     new_item = input("> ")
     if new_item == 'DONE':
         show_list();
-        save_to_file(shoppingList);
+        save_to_file(shopping_list);
         break
     elif new_item == 'HELP':
         show_help()
